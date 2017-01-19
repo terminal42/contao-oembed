745ffe6e53056c6c901a719cd9bc6967cefa259f
Andreas Schempp <andreas.schempp@terminal42.ch>
2017-01-19 14:08:09 +0100

    Added Contao Manager plugin

---
+++ ContaoManager/Plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Terminal42\TwitterBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Terminal42\TwitterBundle\Terminal42TwitterBundle;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            (new BundleConfig(Terminal42TwitterBundle::class))->setLoadAfter([ContaoCoreBundle::class])
+        ];
+    }
+}
