1c8418224b970acb3533aa3621441f0a89f93c03
Andreas Schempp <andreas.schempp@terminal42.ch>
2020-09-22 13:54:29 +0200

    Added missing return type

--- src/Terminal42OEmbedBundle.php
+++ src/Terminal42OEmbedBundle.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class Terminal42OEmbedBundle extends Bundle
 {
-    public function getPath()
+    public function getPath(): string
     {
         return \dirname(__DIR__);
     }
